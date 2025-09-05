23b0ed15f4b06c179b5ec507010f092f4f82aa5b
minhtri1612 <10422119@vgu.edu.vn>
2025-09-05 14:36:28 +0700

    bai19_env

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const path = require('path')
+require('dotenv').config(); // Load biến môi trường từ file .env
+
+console.log("check env:", process.env.PORT);
 
 const app = express()
-const port = 8081
+const port = process.env.PORT || 8081
+const hostname = process.env.HOST_NAME || 'localhost';
 
 app.set('views', path.join(__dirname,'views')); // Thư mục views
 app.set('view engine', 'ejs') // Sử dụng EJS làm view engine
@@ -17,6 +21,6 @@ app.get('/about', (req, res) => {
   res.send('About Page')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(port, hostname, () => {
+  console.log(`Example app listening on http://${hostname}:${port}`)
 })
