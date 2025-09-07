7b944d81edaa7992c6b22ed5f1a77c68ed9f8b73
minhtri1612 <10422119@vgu.edu.vn>
2025-09-07 21:09:06 +0700

    bai 32- connection cost, tạo mới connect

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,21 @@
+const connection = require('../config/db');
+
 const getHomepage = (req, res) => {
-    res.render('sample.ejs')
+
+    let users = [];
+
+    connection.query(
+        'select * from Users u',
+        function(err, results, fields) {
+            users= results;
+            console.log('Query results:', results);
+
+            //console.log("check users:", users)
+            //res.send("Hello tran nguyen minh tri")
+            res.send(JSON.stringify(users))
+        }
+    );
+
 }
 
 const getAboutPage = (req, res) => {
