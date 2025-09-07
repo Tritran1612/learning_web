6078f124d82bcc02d811229c34e32a7573b62878
minhtri1612 <10422119@vgu.edu.vn>
2025-09-07 21:32:52 +0700

    bai 35 - design NavBar

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -2,19 +2,20 @@ const connection = require('../config/db');
 
 const getHomepage = (req, res) => {
 
-    let users = [];
+    // let users = [];
 
-    connection.query(
-        'select * from Users u',
-        function(err, results, fields) {
-            users= results;
-            console.log('Query results:', results);
+    // connection.query(
+    //     'select * from Users u',
+    //     function(err, results, fields) {
+    //         users= results;
+    //         console.log('Query results:', results);
 
-            //console.log("check users:", users)
-            //res.send("Hello tran nguyen minh tri")
-            res.send(JSON.stringify(users))
-        }
-    );
+    //         //console.log("check users:", users)
+    //         //res.send("Hello tran nguyen minh tri")
+    //         res.send(JSON.stringify(users))
+    //     }
+    // );
+    return res.render('home.ejs');
 
 }
 
