20bc4855d9fe2ec4affb6646941915566b0ced00
minhtri1612 <10422119@vgu.edu.vn>
2025-09-07 21:16:50 +0700

    bai 33 - connection pool

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,24 @@
 const mysql = require('mysql2');
 
 //test connection database
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT
+
+// })
+
+const connection = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections:true,
+    connectionLimit: 10,
+    queueLimit:0
 
 })
 
