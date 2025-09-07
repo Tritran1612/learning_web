aaf938d5578b16b4674b51f5e7e6ec3442d5c901
minhtri1612 <10422119@vgu.edu.vn>
2025-09-07 20:03:56 +0700

    using MySQL with NodeJS

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const path = require('path')
 require('dotenv').config(); // Load biến môi trường từ file .env
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
+const mysql = require('mysql2');
 
 console.log("check env:", process.env.PORT);
 
@@ -14,6 +15,27 @@ const hostname = process.env.HOST_NAME || 'localhost';
 configViewEngine(app);
 //config web routes
 app.use('/', webRoutes);
+
+//test connection database
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+
+})
+
+// simple query
+
+connection.query(
+  'select * from Users u',
+  function(err, results, fields) {
+
+    console.log('Query results:', results);
+    console.log('Fields results:', fields);
+  }
+)
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on http://${hostname}:${port}`)
